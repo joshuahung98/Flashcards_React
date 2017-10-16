95bbf9f37dc321b49b04cd6cb9df57aea6afd072
Joshua Hung <joshualh@usc.edu>
2017-10-16 01:46:25 -0700

    Flash cards commit

---
+++ App.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  ScrollView,
+  Modal,
+  TextInput
+} from 'react-native';
+
+/*
+  Import our two custom components
+*/
+import NewCardModal from './NewCardModal';
+import Card from './Card';
+
+/*
+  Default set of cards
+*/
+const DEFAULT_CARDS = [
+  {
+    term: "TERM",
+    definition: "Sample definition"
+  }
+];
+
+class App extends Component {
+  // Fill this out
+  state = {
+    cards: DEFAULT_CARDS,
+    modalVisible: false
+  }
+
+  /*
+    Toggles the new card modal
+  */
+  _toggleModal = () => {
+  this.setState({
+    modalVisible: !this.state.modalVisible
+  });
+}
+
+  /*
+    Passed to the new card modal.
+    Called when user decides to add new card.
+    Creates card object and adds it to our state
+  */
+  _addCard = (_term, _definition) => {
+  const cards = this.state.cards;
+
+  cards.push({
+    term: _term,
+    definition: _definition
+  });
+
+  this.setState({
+    cards: cards
+  });
+
+  this.setState({
+    modalVisible: false
+  });
+}
+
+  render() {
+    // Loop through the cards array in state and create Card component for each card
+    const cards = this.state.cards.map((card, index) => {
+      return (
+        <Card cardData={card} key={index} />
+      )
+    });
+    return (
+  <View style={styles.container}>
+  <NewCardModal
+  modalVisible={this.state.modalVisible}
+  toggleModal={this._toggleModal}
+  addCard={this._addCard}
+/>
+    <ScrollView>
+      {cards}
+    </ScrollView>
+    <TouchableHighlight
+  style={styles.addButton}
+  onPress={this._toggleModal}
+  underlayColor='#128040'
+>
+  <Text style={styles.addButtonText}>Add Card</Text>
+</TouchableHighlight>
+  </View>
+);
+
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f442d7'
+  },
+  addButton: {
+    backgroundColor: '#f442d7',
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+  addButtonText: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontSize: 20
+  }
+});
+
+export default App;
